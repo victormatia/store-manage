460d00e9f8fd6a17dba769d65737fc48ead4d0f3
Victor Matias <fvictor1705@gmail.com>
2022-11-12 18:53:49 -0300

    test: refactor and add new test case

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -12,12 +12,18 @@ describe('Aplica casos de testes a productsModel', function () {
   it('Verificado se retorna todos os produtos disponíveis', async function () {
     sinon.stub(connection, 'execute').resolves(allProductsResponse);
     const result = await productsModel.findAllProducts();
-    expect(result).to.be.deep.equal(allProductsResponse[0]);;
+    expect(result).to.be.deep.equal(allProductsResponse[0]);
   });
   
   it('Verificado se retorna um produto baseado em um id', async function () {
     sinon.stub(connection, 'execute').resolves(productsOne);
     const result = await productsModel.findProductById(1);
-    expect(result).to.be.deep.equal(productsOne[0][0]);;
+    expect(result).to.be.deep.equal(productsOne[0][0]);
+  });
+
+  it('Verificado se retorna "product not found", quando i id não é encontrado', async function () {
+    sinon.stub(connection, 'execute').resolves([[]]);
+    const result = await productsModel.findProductById(13);
+    expect(result).to.be.equal(undefined);
   });
 });
