9dd874632a7715ae9b6feb4a9b3d7f7b28d7353c
Victor Matias <fvictor1705@gmail.com>
2022-11-18 20:01:40 -0300

    test: add a new test case to sales.service

---
+++ tests/unit/services/mock/mockSales.js
@@ -0,0 +1,53 @@
+const allSalesResponse = [
+  {
+    sale_id: 1,
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    product_id: 1,
+    quantity: 1
+  },
+  {
+    sale_id: 2,
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    product_id: 3,
+    quantity: 15
+  }
+];
+
+const salesResponse = [
+  {
+    sale_id: 1,
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    product_id: 1,
+    quantity: 1
+  },
+];
+
+const sale = [
+  {
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    productId: 1,
+    quantity: 1
+  },
+];
+
+const allSales = [
+  {
+    saleId: 1,
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    productId: 1,
+    quantity: 1
+  },
+  {
+    saleId: 2,
+    date: '2022 - 11 - 18T19: 11: 53.000Z',
+    productId: 3,
+    quantity: 15
+  }
+];
+
+module.exports = {
+  allSalesResponse,
+  allSales,
+  salesResponse,
+  sale,
+};
