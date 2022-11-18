3582332d89ab3fbb29b7c3a91c03d4c4bb2b0306
Victor Matias <fvictor1705@gmail.com>
2022-11-18 00:44:43 -0300

    test: apply tests cases to sale.model.js

---
+++ tests/unit/models/mock/mockSalesResponse.js
@@ -0,0 +1,38 @@
+const allSalesResponse = [
+  [
+    {
+      sale_id: 1,
+      date: '2022 - 11 - 17T20: 47: 19.000Z',
+      product_id: 1,
+      quantity: 5
+    },
+    {
+      sale_id: 1,
+      date: '2022 - 11 - 17T20: 47: 19.000Z',
+      product_id: 2,
+      quantity: 10
+    },
+    {
+      sale_id: 2,
+      date: '2022 - 11 - 17T20: 47: 19.000Z',
+      product_id: 3,
+      quantity: 15
+    }
+  ],
+];
+
+const saleResponse = [
+  [
+    {
+      sale_id: 1,
+      date: '2022 - 11 - 17T20: 47: 19.000Z',
+      product_id: 1,
+      quantity: 5
+    },
+  ],
+];
+
+module.exports = {
+  allSalesResponse,
+  saleResponse,
+}
---
+++ tests/unit/models/sales.model.test.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/db/connection');
+const salesModel = require('../../../src/models/sales.model');
+
+const { allSalesResponse, saleResponse } = require('./mock/mockSalesResponse');
+
+const sale = [
+  {
+    "productId": 3,
+    "quantity": 1
+  },
+  {
+    "productId": 3,
+    "quantity": 1
+  }
+];
+
+describe('Aplica casos de teste a salesModel', function () {
+  afterEach(sinon.restore);
+
+  it('Verificado se retorna todas as vendas existentes', async function () { 
+    sinon.stub(connection, 'execute').resolves(allSalesResponse);
+
+    const result = await salesModel.findAllSales();
+
+    expect(result).to.be.deep.equals(allSalesResponse[0]);
+  });
+
+  it('Verificado se retorna uma venda baseado em um id', async function () {
+    sinon.stub(connection, 'execute').resolves(saleResponse);
+
+    const result = await salesModel.findSalesById(12);
+    expect(result).to.be.deep.equals(saleResponse[0]);
+  });
+
+  it('Verificado se é possível adicionar uma nova venda', async function () {
+    sinon.stub(connection, 'execute').resolves([{ insertId: 2 }]);
+
+    const result = await salesModel.postSale(sale);
+    expect(result).to.be.equal(2);
+  });
+
+  it('Verificado se é possível apagar uma venda', async function () {
+    sinon.stub(connection, 'execute').resolves([{ affectedRows: 2 }]);
+
+    const result = await salesModel.deleteSale(1);
+
+    expect(result).to.be.equal(2);
+  });
+});
